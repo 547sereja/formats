23fa840ee58d161eeba0ecf513379904a5325ce5
Sergey Illyushko <serhiiilliushko@gmail.com>
2020-06-16 17:41:06 +0300

    fixed all mistakes

--- prog_for_json.py
+++ prog_for_json.py
@@ -4,13 +4,14 @@ import json
 
 
 def func_split_news_from_json():
+    result = []
     with open('newsafr.json', encoding='utf-8') as file:
         data = json.load(file)
         for all_news in data['rss']['channel']['items']:
             each_word = all_news['description'].lower().split()
+            result.extend(each_word)
+        return result
 
-        return each_word
-# print(func_split_news_from_json())
 
 def look_for_most_common_words(each_word):
     needed_len = 6
@@ -20,8 +21,9 @@ def look_for_most_common_words(each_word):
             more_6_len_list.append(needed_words)
 
     more_6_len_list.sort(key=more_6_len_list.count, reverse=True)
-    for words in more_6_len_list[0:10]:
-        print(f"{len(words)},letters in the word:  {words}")
+    for i, words in enumerate(more_6_len_list[0:10]):
+        print(f"Word number {i + 1}:  {words}")
 
 
 look_for_most_common_words(func_split_news_from_json())
+
--- prog_for_xml.py
+++ prog_for_xml.py
@@ -1,9 +1,8 @@
-import prog_for_json
+from prog_for_json import look_for_most_common_words
+import xml.etree.ElementTree as ET
 
 
 def func_split_news_from_xml():
-    import prog_for_json
-    import xml.etree.ElementTree as ET
     parser = ET.XMLParser(encoding='utf-8')
     tree = ET.parse("newsafr.xml", parser)
     root = tree.getroot()
@@ -13,7 +12,7 @@ def func_split_news_from_xml():
     return each_word
 
 
-prog_for_json.look_for_most_common_words(func_split_news_from_xml())
+look_for_most_common_words(func_split_news_from_xml())
 
 
 
